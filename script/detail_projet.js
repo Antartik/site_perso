e1f2c937d8708bc7cbfa112c429beb155cf62cff
Antartik <antoine.tsuki@gmail.com>
2015-10-27 00:03:30 +0100

    page descriptif projet

---
+++ script/detail_projet.js
@@ -0,0 +1,25 @@
+
+$(".projet").mouseenter(function(){
+	$(this).find(".hover_projet").clearQueue().animate({opacity : 1},300);
+});
+
+$(".projet").mouseleave(function(){
+	$(this).find(".hover_projet").clearQueue().animate({opacity : 0},300);
+});
+
+$("#menu1").click(function(){
+	$("#logoload1, #logoload2, #logoload3, #logoload4").css({display : "none"});
+	$("#chargement").css({opacity : 1});
+	$("#chargement").fadeIn(200);
+	setTimeout(function(){
+		document.location.href="index.html"
+	},200);
+});
+
+$("#menu3").click(function(){
+	$("#chargement").css({opacity : 1});
+	$("#chargement").fadeIn(200);
+	setTimeout(function(){
+		document.location.href="contact.html"
+	},200);
+});
